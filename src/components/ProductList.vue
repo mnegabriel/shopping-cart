eee4d7baa8b9f3e540202ad3e601ae21f6097a1d
Gabriel Dantas <gabrielmd123@gmail.com>
2020-11-09 06:53:13 -0300

    Init

---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="product-list">
+      <h1>Product List</h1>
+      <ul>
+        <li v-for="product of products" :key='product.name'>
+          {{product.title}} - R${{product.price}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop.js'
+export default {
+  name: 'ProductList',
+  data() {
+    return {
+      products: []
+      }
+  },
+  created() {
+    shop.getProducts( products => this.products = products)
+  }
+
+}
+</script>
+
+<style>
+
+</style>
